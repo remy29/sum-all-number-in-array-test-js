2409eaeeb03aa8d15625891f6a4d4e0d708c0c1f
remy29 <arthur.d.remy@gmail.com>
2020-10-31 02:28:48 +0000

    last assignment submission before the weekend!

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
 
+const sumItems = function(array) {
+  let sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
+};
+
+console.log(sumItems([1,2,[3,4],5,[6,7]]));
 module.exports = sumItems;
+
